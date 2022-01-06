691861f22702b665783ee393f9271b7f3a4391c8
imranahnaf <mdemranhossen147@gmail.com>
2022-01-06 20:41:05 +0600

    shope section complete

--- js/costom.js
+++ js/costom.js
@@ -1,4 +1,34 @@
 $(function() {
 
+    $(".banner-slider").slick({
 
+        // normal options...
+        infinite: true,
+        prevArrow: '<i class="fa fa-angle-left prevarrow"></i>',
+        nextArrow: '<i class="fa fa-angle-right nextarrow"></i>',
+
+        // the magic
+        responsive: [{
+
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 3,
+                infinite: true
+            }
+
+        }, {
+
+            breakpoint: 600,
+            settings: {
+                slidesToShow: 2,
+                dots: true
+            }
+
+        }, {
+
+            breakpoint: 300,
+            settings: "unslick" // destroys slick
+
+        }]
+    });
 });
